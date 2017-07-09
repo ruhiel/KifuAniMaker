85b52913dd2e8af93c2b89c23fd1f1174739a37b
ruhiel_murrue <ruhiel1983@gmail.com>
2017-07-09 17:43:29 +0900

    プロジェクト ファイルを追加します。

---
+++ KifuAniMaker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KifuAniMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
