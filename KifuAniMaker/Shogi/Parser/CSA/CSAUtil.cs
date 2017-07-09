83ac8956367bd3e234ccf48cec20e00c4edbda02
ruhiel_murrue <ruhiel1983@gmail.com>
2017-07-10 00:12:41 +0900

    WIP

--- KifuAniMaker/Shogi/Parser/CSA/CSAUtil.cs
+++ KifuAniMaker/Shogi/Parser/CSA/CSAUtil.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KifuAniMaker.Shogi.Pieces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,32 @@ using System.Threading.Tasks;
 
 namespace KifuAniMaker.Shogi.Parser.CSA
 {
-    public class CSAUtil
+    public static class CSAUtil
     {
+        private static Dictionary<string, Type> _Map = new Dictionary<string, Type>()
+        {
+            { "", typeof(Bishop) }
+        };
+
+        public static Piece CreatePiece(string piece, BlackWhite bw)
+        {
+            var type = _Map[piece];
+            return (Piece)Activator.CreateInstance(type, bw);
+        }
+        public static BlackWhite ToBlackWhite(this string str)
+        {
+            if(str == "+")
+            {
+                return BlackWhite.Black;
+            }
+            else if(str == "-")
+            {
+                return BlackWhite.White;
+            }
+            else
+            {
+                throw new ArgumentException(str);
+            }
+        }
     }
 }
--- KifuAniMaker/Shogi/Parser/CSA/SetPositionBulk.cs
+++ KifuAniMaker/Shogi/Parser/CSA/SetPositionBulk.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace KifuAniMaker.Shogi.Parser.CSA
 {
     public class SetPositionBulk : ICSAStatement
     {
+        public int Y { get; set; }
+
+        public IEnumerable<string> Pieces { get; set; }
+
         public SetPositionBulk(int y, IEnumerable<string> pieces)
         {
-
+            Y = y;
+            Pieces = pieces;
         }
         public Board Execute(Board board)
         {
-
+            var regex = new Regex(@"(?<bw>\-|\+)(?<piece>..)");
+            for (var i = 0; i < 9; i++)
+            {
+                var match = regex.Match(Pieces.ElementAt(i));
+                if(match.Success)
+                {
+                    var bw = match.Groups["bw"].Value.ToBlackWhite();
+                }
+            }
             throw new NotImplementedException();
         }
     }
